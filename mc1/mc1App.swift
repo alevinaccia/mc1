fa06042172e407cbd12d24e48023f54555f95ed2
alevinaccia <vinacciaalessandro@gmail.com>
2022-10-20 12:51:01 +0200

    Initial Commit

---
+++ mc1/mc1App.swift
@@ -0,0 +1,17 @@
+//
+//  mc1App.swift
+//  mc1
+//
+//  Created by Alessandro Vinaccia on 20/10/22.
+//
+
+import SwiftUI
+
+@main
+struct mc1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
