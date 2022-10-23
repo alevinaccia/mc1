3c758def7d2c40e454dd85aaf0a342b8a154cb2d
alevinaccia <vinacciaalessandro@gmail.com>
2022-10-23 08:49:34 +0200

    Dashboard skeleton

--- mc1/ContentView.swift
+++ mc1/ContentView.swift
@@ -9,13 +9,74 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Giovanni was here")
+        NavigationView{
+            VStack(alignment: .leading){
+                List{
+                    HStack{
+                        NavigationLink {
+                            //page
+                        } label: {
+                            Image(systemName: "globe").resizable().frame(width: 50, height: 50)
+                            Text("Placeholder txt")
+                        }
+                    }
+                }.frame(height: 80)
+                HStack{
+                    Text("Tasks").font(.title2)
+                    Spacer()
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "plus")
+                    }.padding(.trailing)
+                }
+                .padding(.leading).frame(height: 0)
+                
+                Section{
+                    
+                    List{
+                        Section(header: Text("Today"))
+                        {
+                            HStack{
+                                Image(systemName: "globe")
+                                Text("Test")
+                            }
+                            HStack{
+                                Image(systemName: "globe")
+                                Text("Test")
+                            }
+                            HStack{
+                                Image(systemName: "globe")
+                                Text("Test")
+                            }
+                        }
+                        
+                        
+                        Section(header: Text("Tomorrow"))
+                        {
+                            HStack{
+                                Image(systemName: "globe")
+                                Text("Test")
+                            }
+                            HStack{
+                                Image(systemName: "globe")
+                                Text("Test")
+                            }
+                            HStack{
+                                Image(systemName: "globe")
+                                Text("Test")
+                            }
+                        }
+                        
+                    }
+                }
+                
+                
+            }.navigationTitle("Dashboard")
+                .background(Color(red: 0.949, green: 0.949, blue: 0.97, opacity: 1.0))
+            
         }
-        .padding()
+        
     }
 }
 
