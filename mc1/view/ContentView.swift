dc8ca15b0075a5d02b1d9991e1507623b4e97733
Katia <kdelucia22@fed.idserve.net>
2022-10-26 12:40:12 +0200

    bracket

--- mc1/view/ContentView.swift
+++ mc1/view/ContentView.swift
@@ -22,7 +22,17 @@ struct ContentView: View {
                     HStack{
                         NavigationLink {
                             ProfileDetail()
-                        } label: {
+                        } // finish Navigation Link
+                    label: {
+                        HStack {
+                            Image(systemName: "person.fill").resizable().frame(width: 40, height: 40)
+                        } // finish HStack
+                        VStack{
+                            HStack{
+                                Text("Placeholder Profile")
+                                    .font(.title2)
+                                Spacer()
+                            } // finish HStack
                             HStack {
                                 Image(systemName: "person.fill").resizable().frame(width: 40, height: 40)
                             } // finish HStack
@@ -48,77 +58,78 @@ struct ContentView: View {
                             }
                         }
                     }
-                }.frame(height: 80)
-                HStack{
-                    Text("Tasks").font(.title2)
-                    Spacer()
-                    Button{
-                        showingSheet.toggle()
-                    } label: {
-                        Image(systemName: "plus")
-                    }.padding(.trailing)
-                        .sheet(isPresented: $showingSheet) {
-                            AddTask(taskVM: tasksVM)
-                        }
-                    
-                }
-                .padding(.leading).frame(height: 0)
-                
-                List{
-                    Section(header: Text("Today"))
-                    {
-                        ForEach($tasksVM.tasks) { $task in
-                            if task.day == .today{
-                                taskView(task: $task)
+                    }.frame(height: 80)
+                    HStack{
+                        Text("Tasks").font(.title2)
+                        Spacer()
+                        Button{
+                            showingSheet.toggle()
+                        } label: {
+                            Image(systemName: "plus")
+                        }.padding(.trailing)
+                            .sheet(isPresented: $showingSheet) {
+                                AddTask(taskVM: tasksVM)
                             }
-                        }.onDelete(perform: tasksVM.remove)
+                        
                     }
+                    .padding(.leading).frame(height: 0)
                     
-                    
-                    Section(header: Text("Tomorrow"))
-                    {
-                        ForEach($tasksVM.tasks) { $task in
-                            if task.day == .tomorrow{
-                                HStack{
+                    List{
+                        Section(header: Text("Today"))
+                        {
+                            ForEach($tasksVM.tasks) { $task in
+                                if task.day == .today{
                                     taskView(task: $task)
                                 }
-                            }
-                        }.onDelete(perform: tasksVM.remove)
+                            }.onDelete(perform: tasksVM.remove)
+                        }
+                        
+                        
+                        Section(header: Text("Tomorrow"))
+                        {
+                            ForEach($tasksVM.tasks) { $task in
+                                if task.day == .tomorrow{
+                                    HStack{
+                                        taskView(task: $task)
+                                    }
+                                }
+                            }.onDelete(perform: tasksVM.remove)
+                        }
                     }
-                }
-                HStack{
-                    Text("Challenges").font(.title2)
-                    Spacer()
-                    Button{
-                        showingChallenges.toggle()
-                    } // finish Button
-                label: {
-                    Image(systemName: "plus")
-                } // finish label
-                .padding(.trailing)
-                .sheet(isPresented: $showingChallenges) {
-                    AddChallenge(challengeVM: challengesVM)
-                } // finish sheet
-                } // finish HStack
-                .padding([.leading, .bottom]).frame(height: 0)
-                
-                List{
-                    ForEach (challengesVM.challenge){challenge in
-                        if challenge.active == true{
-                            HStack  {
-                                Text(challenge.title)
-                                    .foregroundColor(Color.white)
-                            } // finish HStack
-                        } // finish true
-                    } // finish For Each
-                    .listRowBackground(Color.black)
+                    HStack{
+                        Text("Challenges").font(.title2)
+                        Spacer()
+                        Button{
+                            showingChallenges.toggle()
+                        } // finish Button
+                    label: {
+                        Image(systemName: "plus")
+                    } // finish label
+                    .padding(.trailing)
+                    .sheet(isPresented: $showingChallenges) {
+                        AddChallenge(challengeVM: challengesVM)
+                    } // finish sheet
+                    } // finish HStack
+                    .padding([.leading, .bottom]).frame(height: 0)
                     
-                } // finish List
-            } // finish VStack
-            .scrollDisabled(true)
-            .navigationTitle("Dashboard")
-            .background(Color(red: 0.949, green: 0.949, blue: 0.97, opacity: 1.0))
-        } // finish Navigation View
+                    List{
+                        ForEach (challengesVM.challenge){challenge in
+                            if challenge.active == true{
+                                HStack  {
+                                    Text(challenge.title)
+                                        .foregroundColor(Color.white)
+                                } // finish HStack
+                            } // finish true
+                        } // finish For Each
+                        .listRowBackground(Color.black)
+                        
+                    } // finish List
+                } // finish VStack
+                .scrollDisabled(true)
+                .navigationTitle("Dashboard")
+                .background(Color(red: 0.949, green: 0.949, blue: 0.97, opacity: 1.0))
+            }
+        }// finish Navigation View
         
     }
 }
